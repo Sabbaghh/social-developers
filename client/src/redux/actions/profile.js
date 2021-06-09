79b77eae4c5beea7f4f6ff2ccea5a4161d838041
Sabbaghh <afsabbagh9@gmail.com>
2021-06-09 03:57:20 +0300

    Implementing GET_PROFILES/GET_REPOS/GET_SINGLE PROFILE actions and reducers

--- client/src/redux/actions/profile.js
+++ client/src/redux/actions/profile.js
@@ -11,12 +11,62 @@ const {
 	DELETE_EXPERIENCE,
 	DELETE_USER,
 	CLEAR_PROFILE,
+	GET_PROFILES,
+	GET_REPOS,
 } = constants
 
 //get current user profile
 export const getProfile = () => async (dispatch) => {
 	try {
 		const res = await axios.get('api/profile/me')
+		dispatch({
+			type: GET_PROFILE,
+			payload: res.data,
+		})
+	} catch (err) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status },
+		})
+	}
+}
+//GET ALL PROFILES
+export const getProfiles = () => async (dispatch) => {
+	dispatch({ type: CLEAR_PROFILE })
+	try {
+		const res = await axios.get('api/profile')
+		dispatch({
+			type: GET_PROFILES,
+			payload: res.data,
+		})
+	} catch (err) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status },
+		})
+	}
+}
+
+//GET PROFILE BY ID
+export const getProfilesByID = (id) => async (dispatch) => {
+	try {
+		const res = await axios.get(`api/profile/user/${id}`)
+		dispatch({
+			type: GET_REPOS,
+			payload: res.data,
+		})
+	} catch (err) {
+		dispatch({
+			type: PROFILE_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status },
+		})
+	}
+}
+
+//GET GITHUB REPOS
+export const getGithubRepos = (username) => async (dispatch) => {
+	try {
+		const res = await axios.get(`api/profile/github/${username}`)
 		dispatch({
 			type: GET_PROFILE,
 			payload: res.data,
--- client/src/redux/reducers/profile.js
+++ client/src/redux/reducers/profile.js
@@ -15,6 +15,8 @@ const {
 	ADD_EXPERIENCE,
 	DELETE_EDUCATION,
 	DELETE_EXPERIENCE,
+	GET_PROFILES,
+	GET_REPOS,
 } = constants
 
 const profile = (state = intialState, action) => {
@@ -26,6 +28,10 @@ const profile = (state = intialState, action) => {
 		case DELETE_EDUCATION:
 		case DELETE_EXPERIENCE:
 			return { ...state, profile: payload, loading: false, errors: {} }
+		case GET_PROFILES:
+			return { ...state, profiles: payload, loading: false, errors: {} }
+		case GET_REPOS:
+			return { ...state, repos: payload, loading: false, errors: {} }
 		case CLEAR_PROFILE:
 			return { ...state, profile: null, repos: [], loading: false }
 		case PROFILE_ERROR:

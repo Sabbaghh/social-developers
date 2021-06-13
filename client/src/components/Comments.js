4e5b872f4e46f1bd66161dabb1968edbb74488a0
Sabbaghh <afsabbagh9@gmail.com>
2021-06-13 17:51:25 +0300

    Implementing comment component

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -1,28 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Moment from 'react-moment'
 
-const Comments = ({ comments }) => {
+const Comments = ({ comment }) => {
 	return (
 		<>
 			<div className='p-1 my-1 bg-white post'>
 				<div>
-					<Link to='profile.html'>
-						<img
-							className='round-img'
-							src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200'
-							alt=''
-						/>
-						<h4>John Doe</h4>
+					<Link to={`/profile/${comment.user}`}>
+						<img className='round-img' src={comment.avatar} alt='avatar' />
+						<h4>{comment.name}</h4>
 					</Link>
 				</div>
 				<div>
-					<p className='my-1'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-						possimus corporis sunt necessitatibus! Minus nesciunt soluta
-						suscipit nobis. Amet accusamus distinctio cupiditate blanditiis
-						dolor? Illo perferendis eveniet cum cupiditate aliquam?
-					</p>
-					<p className='post-date'>Posted on 04/16/2019</p>
+					<p className='my-1'>{comment.text}</p>
+					<Moment className='post-date' format='YYYY/MM/DD'>
+						{comment.date}
+					</Moment>
 				</div>
 			</div>
 		</>
